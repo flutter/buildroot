36da281753fd1cac2bb6742bb68b9d29e95bdec1
Ben Konyi <bkonyi@google.com>
2018-10-16 15:44:12 -0700

    Addressed comments, updated paths to be platform independent

--- tools/dart/dart_roll_helper.py
+++ tools/dart/dart_roll_helper.py
@@ -15,7 +15,12 @@
 #     'flutter/ci/licenses_golden'
 #   - Generating a commit with relevant Dart SDK commit logs (optional)
 #
+<<<<<<< HEAD
 # The following environment variables can be set:
+=======
+# The following environment variables can be set instead of being passed as
+# arguments:
+>>>>>>> Addressed comments, updated paths to be platform independent
 #   - FLUTTER_HOME: the absolute path to the 'flutter' directory
 #   - ENGINE_HOME: the absolute path to the 'engine/src' directory
 #   - DART_SDK_HOME: the absolute path to the root of a Dart SDK project
@@ -24,6 +29,10 @@ import argparse
 import datetime
 import fileinput
 import os
+<<<<<<< HEAD
+=======
+import platform
+>>>>>>> Addressed comments, updated paths to be platform independent
 import shutil
 import subprocess
 import sys
@@ -34,6 +43,10 @@ def env_var(var):
   except KeyError:
     return ''
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> Addressed comments, updated paths to be platform independent
 FLUTTER_HOME = env_var('FLUTTER_HOME')
 ENGINE_HOME = env_var('ENGINE_HOME')
 DART_SDK_HOME = env_var('DART_SDK_HOME')
@@ -44,50 +57,64 @@ ERROR_GCLIENT_SYNC_FAILED    = 1
 ERROR_BUILD_FAILED           = 2
 ERROR_PKG_FLUTTER_FAILED     = 3
 ERROR_FLUTTER_GALLERY_FAILED = 4
-
-def engine_golden_licenses_path():
-  return os.path.join(ENGINE_HOME, 'flutter/ci/licenses_golden/')
-
-def engine_license_script_path():
-  return os.path.join(ENGINE_HOME, 'flutter/ci/licenses.sh')
-
-def engine_flutter_path():
-  return os.path.join(ENGINE_HOME, 'flutter')
-
-def flutter_deps_path():
-  return os.path.join(ENGINE_HOME, 'flutter/DEPS')
-
-def flutter_gallery_path():
-  return os.path.join(FLUTTER_HOME, 'examples/flutter_gallery')
-
-def license_script_output_path():
-  return os.path.join(ENGINE_HOME, 'out/license_script_output/')
-
-def package_flutter_path():
-  return os.path.join(FLUTTER_HOME, 'packages/flutter')
-
-def update_dart_deps_path():
-  return os.path.join(ENGINE_HOME, 'tools/dart/create_updated_flutter_deps.py')
+ERROR_MISSING_ROOTS          = 5
 
 DART_REVISION_ENTRY = 'dart_revision'
 FLUTTER_RUN = ['flutter', 'run']
 FLUTTER_TEST = ['flutter', 'test']
 
-original_revision = ''
-updated_revision = ''
+def engine_golden_licenses_path():
+  return os.path.join(ENGINE_HOME, 'flutter', 'ci', 'licenses_golden')
+
+
+def engine_license_script_path():
+  return os.path.join(ENGINE_HOME, 'flutter', 'ci', 'licenses.sh')
+
+
+def engine_flutter_path():
+  return os.path.join(ENGINE_HOME, 'flutter')
+
+
+def flutter_deps_path():
+  return os.path.join(ENGINE_HOME, 'flutter', 'DEPS')
+
+
+def flutter_gallery_path():
+  return os.path.join(FLUTTER_HOME, 'examples', 'flutter_gallery')
+
+
+def license_script_output_path():
+  return os.path.join(ENGINE_HOME, 'out', 'license_script_output')
+
+
+def package_flutter_path():
+  return os.path.join(FLUTTER_HOME, 'packages', 'flutter')
+
+
+def update_dart_deps_path():
+  return os.path.join(ENGINE_HOME, 'tools', 'dart', 'create_updated_flutter_deps.py')
+
 
 def print_status(msg):
   CGREEN = '\033[92m'
   CEND = '\033[0m'
   print(CGREEN + '[STATUS] ' + msg + CEND)
 
-def print_error(msg):
-  CGREEN = '\033[91m'
+
+def print_warning(msg):
+  CYELLOW = '\033[93m'
   CEND = '\033[0m'
-  print(CGREEN + '[ERROR] ' + msg + CEND)
+  print(CYELLOW + '[WARNING] ' + msg + CEND)
+
+
+def print_error(msg):
+  CRED = '\033[91m'
+  CEND = '\033[0m'
+  print(CRED + '[ERROR] ' + msg + CEND)
+
 
 def update_dart_revision(dart_revision):
-  global original_revision
+  original_revision = ''
   print_status('Updating Dart revision to {}'.format(dart_revision))
   content = get_deps()
   for idx, line in enumerate(content):
@@ -96,13 +123,16 @@ def update_dart_revision(dart_revision):
       content[idx] = "  'dart_revision': '" + dart_revision + "',\n"
       break
   write_deps(content)
+  return original_revision
+
 
 def gclient_sync():
   exit_code = None
   num_retries = 0
 
   while ((exit_code != 0) and not (num_retries >= MAX_GCLIENT_RETRIES)):
-    print_status('Running gclient sync')
+    print_status('Running gclient sync (Attempt {}/{})'
+                 .format(num_retries + 1, MAX_GCLIENT_RETRIES))
     p = subprocess.Popen(['gclient', 'sync'], cwd=ENGINE_HOME)
     exit_code = p.wait()
     if exit_code != 0:
@@ -117,18 +147,21 @@ def get_deps():
     content = f.readlines()
   return content
 
+
 def update_deps():
   print_status('Updating Dart dependencies')
   p = subprocess.Popen([update_dart_deps_path()], cwd=ENGINE_HOME)
   p.wait()
 
+
 def write_deps(newdeps):
   with open(flutter_deps_path(), 'w') as f:
     f.write(''.join(newdeps))
 
+
 def run_gn():
   print_status('Generating build files')
-  common = ['flutter/tools/gn', '--goma']
+  common = [os.path.join('flutter', 'tools', 'gn'), '--goma']
   debug = ['--runtime-mode=debug']
   profile = ['--runtime-mode=profile']
   release = ['--runtime-mode=release']
@@ -137,38 +170,46 @@ def run_gn():
   android = ['--android']
 
   for mode in runtime_modes:
-    p = subprocess.Popen(common + android + unopt + mode, cwd=ENGINE_HOME)
+    if set(mode) != set(release):
+      p = subprocess.Popen(common + android + unopt + mode, cwd=ENGINE_HOME)
+      p.wait()
     q = subprocess.Popen(common + android + mode, cwd=ENGINE_HOME)
     host = common[:]
     if set(mode) == set(debug):
       host += unopt
     r = subprocess.Popen(host + mode, cwd=ENGINE_HOME)
-    p.wait()
     q.wait()
     r.wait()
+
 
 def build():
   print_status('Building Flutter engine')
   command = ['ninja', '-j1000']
   configs = [
-    'out/host_debug_unopt',
-    'out/host_release',
-    'out/host_profile',
-    'out/android_debug_unopt',
-    'out/android_debug',
-    'out/android_profile_unopt',
-    'out/android_profile',
-    'out/android_release_unopt',
-    'out/android_release'
+    'host_debug_unopt',
+    'host_release',
+    'host_profile',
+    'android_debug_unopt',
+    'android_debug',
+    'android_profile_unopt',
+    'android_profile',
+    'android_release'
   ]
+
+  build_dir = 'out'
+  if platform.system() == 'Darwin':
+    build_dir = 'xcodebuild'
+
   for config in configs:
-    p = subprocess.Popen(command + ['-C', config], cwd=ENGINE_HOME)
+    p = subprocess.Popen(command + ['-C', os.path.join(build_dir, config)],
+                         cwd=ENGINE_HOME)
     error_code = p.wait()
     if error_code != 0:
       print_error('Build failure for configuration "' +
                   config +
                   '". Aborting roll.')
       sys.exit(ERROR_BUILD_FAILED)
+
 
 def run_tests():
   print_status('Running tests in packages/flutter')
@@ -187,6 +228,7 @@ def run_tests():
     print_error('flutter_gallery tests failed. Aborting roll.')
     sys.exit(ERROR_FLUTTER_GALLERY_FAILED)
 
+
 def run_hot_reload_configurations():
   print_status('Running flutter gallery release')
   p = subprocess.Popen(FLUTTER_RUN + ['--release', '--local-engine=android_release'],
@@ -196,6 +238,7 @@ def run_hot_reload_configurations():
   p = subprocess.Popen(FLUTTER_RUN + ['--local-engine=android_debug_unopt'],
                        cwd=flutter_gallery_path())
   p.wait()
+
 
 def update_licenses():
   print_status('Updating Flutter licenses')
@@ -207,6 +250,7 @@ def update_licenses():
     if os.path.isfile(path):
       shutil.copy(path, engine_golden_licenses_path())
 
+
 def get_commit_range(start, finish):
   range_str = '{}...{}'.format(start, finish)
   command = ['git', 'log', '--oneline', range_str]
@@ -216,10 +260,8 @@ def get_commit_range(start, finish):
   os.chdir(orig_dir)
   return result
 
-def git_commit():
-  global original_revision
-  global updated_revision
 
+def git_commit(original_revision, updated_revision):
   print_status('Committing Dart SDK roll')
   current_date = datetime.date.today()
   sdk_log = get_commit_range(original_revision, updated_revision)
@@ -228,6 +270,7 @@ def git_commit():
   commit_cmd = ['git', 'commit', '-a', '-m', commit_msg]
   p = subprocess.Popen(commit_cmd, cwd=engine_flutter_path())
   p.wait()
+
 
 def update_roots(args):
   global FLUTTER_HOME
@@ -243,9 +286,23 @@ def update_roots(args):
   if args.dart_sdk_home:
     DART_SDK_HOME = args.dart_sdk_home
 
-def main():
-  global updated_revision
+  if FLUTTER_HOME == '':
+    print_error('Either "--flutter_home" must be provided or FLUTTER_HOME must' +
+                ' be set. Aborting roll.')
+    sys.exit(ERROR_MISSING_ROOTS)
 
+  if ENGINE_HOME == '':
+    print_error('Either "--engine_home" must be provided or ENGINE_HOME must' +
+                ' be set. Aborting roll.')
+    sys.exit(ERROR_MISSING_ROOTS)
+
+  if DART_SDK_HOME == '':
+    print_error('Either "--dart_sdk_home" must be provided or DART_SDK_HOME ' +
+                'must be set. Aborting roll.')
+    sys.exit(ERROR_MISSING_ROOTS)
+
+
+def main():
   parser = argparse.ArgumentParser(description='Automate most Dart SDK roll tasks.')
   parser.add_argument('--dart_sdk_home', help='Path to the Dart SDK ' +
                       'repository. Overrides DART_SDK_HOME environment variable')
@@ -269,13 +326,15 @@ def main():
                       help='Skip updating licenses')
 
   args = parser.parse_args()
+
+  original_revision = None
   updated_revision = args.dart_sdk_revision
 
   update_roots(args)
 
   print_status('Starting Dart SDK roll')
   if not args.no_update_deps:
-    update_dart_revision(updated_revision)
+    original_revision = update_dart_revision(updated_revision)
     gclient_sync()
     update_deps()
     gclient_sync()
@@ -289,8 +348,14 @@ def main():
   if not args.no_update_licenses:
     update_licenses()
   if args.create_commit:
-    git_commit()
+    if original_revision == None:
+      print_warning('"original_revision" not specified. Skipping commit.')
+      print_warning('This happens when the "--no_update_deps" argument is ' +
+                    'provided')
+    else:
+      git_commit(original_revision, updated_revision)
   print_status('Dart SDK roll complete!')
+
 
 if __name__ == '__main__':
   main()
