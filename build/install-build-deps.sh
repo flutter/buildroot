e7c3aecac1f8888c08324152098fc92a26240053
Hugo van der Merwe <hugovdm@gmail.com>
2019-04-03 17:11:04 +0200

    Add libgtk-3-dev to dev_list.

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -93,10 +93,10 @@ dev_list="bison cdbs curl dpkg-dev elfutils devscripts fakeroot
           libasound2-dev libbrlapi-dev
           libbz2-dev libcairo2-dev libcap-dev libcups2-dev libcurl4-gnutls-dev
           libdrm-dev libelf-dev libexif-dev libgconf2-dev libglib2.0-dev
-          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libkrb5-dev
-          libnspr4-dev libnss3-dev libpam0g-dev libpci-dev libpulse-dev
-          libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev libudev-dev
-          libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
+          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libgtk-3-dev
+          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libpci-dev
+          libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
+          libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
           patch perl pkg-config python python-cherrypy3 python-crypto
           python-dev python-numpy python-opencv python-openssl python-psutil
           python-yaml rpm ruby subversion wdiff zip"
